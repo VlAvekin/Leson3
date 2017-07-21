96cb95f250b7fb2bd1ecf50748c5c68181f73796
VlAvekin <vladaavekin@mail.com>
2017-07-21 12:08:39 +0300

    Home Work Tasc 7

--- src/com/gmail/vladaavekin/HomeWork/Tasc7.java
+++ src/com/gmail/vladaavekin/HomeWork/Tasc7.java
@@ -1,4 +1,33 @@
 package com.gmail.vladaavekin.HomeWork;
 
+//  Найти 15 первых натуральных чисел, делящихся нацело на 19 и больших 100.
+
 public class Tasc7 {
+
+    public static void main(String[] args) {
+
+        int a = 19;
+        int N = 15;
+        int large = 100;
+
+        boolean counter = true;
+
+        int res = 0;
+
+        for (int i = large; counter; i++){
+
+            if ((i % a) == 0) {
+                res++;
+                System.out.print(i + "\t");
+                if (res == N) {
+                    counter = false;
+                    System.out.println("\n" + res);
+                }
+            }
+
+        }
+
+
+    }
+
 }
