11658fa3260077f5fe6a267c9bcd43aed2045976
VlAvekin <vladaavekin@mail.com>
2017-07-21 11:40:46 +0300

    Home Work Tasc 10

--- src/com/gmail/vladaavekin/HomeWork/Tasc10.java
+++ src/com/gmail/vladaavekin/HomeWork/Tasc10.java
@@ -1,5 +1,22 @@
 package com.gmail.vladaavekin.HomeWork;
 
+// Дана арифметическая прогрессия 1, 5, 9, 13, ...
+// Написать программу определения суммы первых десяти
+// ее членов. Вывести значение суммы и всех ее слагаемых.
 
 public class Tasc10 {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i = 1; i < 40; i += 4) {
+            System.out.print(i + "\t");
+            sum += i;
+        }
+
+        System.out.println("\n" + sum);
+
+    }
+
 }
