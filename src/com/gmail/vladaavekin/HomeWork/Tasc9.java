284d0d3520bfe9f95c108615263bfb4fb27523db
VlAvekin <vladaavekin@mail.com>
2017-07-21 11:55:13 +0300

    Home Work Tasc 9

--- src/com/gmail/vladaavekin/HomeWork/Tasc9.java
+++ src/com/gmail/vladaavekin/HomeWork/Tasc9.java
@@ -1,6 +1,44 @@
 package com.gmail.vladaavekin.HomeWork;
 
+//  Дана арифметическая прогрессия с параметрами а = 2, b = 4.
+// Сколько нужно взять членов прогрессии, начиная с первого,
+// чтобы их сумма превысила заданную величину Z. Написать программу
+// решения задачи. Вывести искомое число слагаемых, величину суммы и последнее слагаемое.
 
+import java.util.Scanner;
 
 public class Tasc9 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double a = 2;
+        double b = 4;
+
+        System.out.print("Величина Z = ");
+        double Z = scanner.nextDouble();
+
+        double step = (a + b) / Z;
+        double sum = 0;
+
+        int slogCount = 1;
+
+        double slogP = 0;
+
+        for (double i = a; i < b; i += step) {
+            sum += i;
+            slogCount++;
+
+            if (i < b) slogP = i;
+
+        }
+
+
+        System.out.println("число слагаемых = " + slogCount);
+        System.out.println("Сумма = " + sum);
+        System.out.println("последнее слагаемое = " + slogP);
+
+    }
+
 }
